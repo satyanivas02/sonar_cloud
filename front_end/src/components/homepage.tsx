c9c40d6982c4f3a2d40ae2ea68d0daa26859e9ea
satyanivas02 <satyanivas.miryala@zemosolabs.com>
2024-10-23 09:44:21 +0530

    created a branch with FE_sonar

---
+++ front_end/src/components/homepage.tsx
@@ -0,0 +1,10 @@
+const HomePage = () => {
+    return (
+      <div>
+          <h2>Home Page</h2>
+          <p>This is the sample home page </p>
+      </div>
+    )
+  }
+  
+  export default HomePage
